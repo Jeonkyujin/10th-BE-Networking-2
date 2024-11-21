a0f1f58d45faf1e4f0e4425a167083032ca92a6a
Jeonkyujin <954qew@naver.com>
2024-11-21 18:15:22 +0900

    feat: Post 엔티티, PostDTO 생성

--- backend/src/main/java/cotato/backend/domains/post/Post.java
+++ backend/src/main/java/cotato/backend/domains/post/Post.java
@@ -1,13 +1,43 @@
 package cotato.backend.domains.post;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.stereotype.Service;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
 
+	@Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String content;
+
+	@Column(nullable = false)
+	private String name;
+
+	@ColumnDefault("0")
+	private int views;
+
+	public Post(String title, String content, String name) {
+		this.title = title;
+		this.content = content;
+		this.name = name;
+	}
 }
---
+++ backend/src/main/java/cotato/backend/domains/post/PostDTO.java
@@ -0,0 +1,24 @@
+package cotato.backend.domains.post;
+
+public class PostDTO {
+
+	private String title;
+
+	private String content;
+
+	private String name;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+}
